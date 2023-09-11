5cd757d9145eb2b65475df36ea955bda23f3de65
SaberCon <894876785@qq.com>
2023-09-11 17:04:29 +0800

    Add another solution for reverse-vowels-of-a-string

--- leetcode-75/reverse-vowels-of-a-string.py
+++ leetcode-75/reverse-vowels-of-a-string.py
@@ -3,3 +3,19 @@ class Solution:
         vowels = [(i, c) for i, c in enumerate(s) if c in 'aeiouAEIOU']
         reversed_vowels = {i: c for (_, c), (i, _) in zip(vowels, reversed(vowels))}
         return ''.join(reversed_vowels.get(i, c) for i, c in enumerate(s))
+
+
+class Solution2:
+    def reverseVowels(self, s: str) -> str:
+        chars = list(s)
+        start, end = 0, len(s) - 1
+        while start < end:
+            if chars[start] not in 'aeiouAEIOU':
+                start += 1
+            elif chars[end] not in 'aeiouAEIOU':
+                end -= 1
+            else:
+                chars[start], chars[end] = chars[end], chars[start]
+                start += 1
+                end -= 1
+        return ''.join(chars)
