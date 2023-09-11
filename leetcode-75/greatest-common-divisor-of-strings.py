a1f7f4f7c5e40ea20286aece7da6b49a55c2eb10
SaberCon <894876785@qq.com>
2023-09-11 16:05:56 +0800

    Finish greatest-common-divisor-of-strings

---
+++ leetcode-75/greatest-common-divisor-of-strings.py
@@ -0,0 +1,10 @@
+from math import gcd
+
+
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        """ Definitely not an easy problem. """
+
+        if str1 + str2 != str2 + str1:
+            return ''
+        return str1[:gcd(len(str1), len(str2))]
