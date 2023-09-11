304d8d59e10d07686839de48adcf100786e874cd
SaberCon <894876785@qq.com>
2023-09-11 13:07:48 +0800

    Add default imported modules cheatsheet

---
+++ imported.py
@@ -0,0 +1,19 @@
+from bisect import *
+from collections import *
+from heapq import *
+from itertools import *
+from math import *
+from random import *
+from re import *
+from statistics import *
+
+import bisect
+import collections
+import heapq
+import itertools
+import math
+import random
+import re
+import statistics
+
+from typing import *
