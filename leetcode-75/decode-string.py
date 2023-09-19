61c1cd4d2061f0dfcf98edfdd701274e7393fe27
SaberCon <894876785@qq.com>
2023-09-19 08:55:55 +0800

    Add another solution for decode-string

--- leetcode-75/decode-string.py
+++ leetcode-75/decode-string.py
@@ -19,3 +19,22 @@ class Solution:
 
             stack.append(string * number)
         return ''.join(stack)
+
+
+class Solution2:
+    def decodeString(self, s: str) -> str:
+        strs = ['']
+        nums = [0]
+        for c in s:
+            if c.isdigit():
+                nums[-1] = nums[-1] * 10 + int(c)
+            elif c.isalpha():
+                strs[-1] += c
+            elif c == '[':
+                strs.append('')
+                nums.append(0)
+            elif c == ']':
+                decoded = strs.pop() * nums.pop(-2)
+                strs[-1] += decoded
+        assert len(strs) == 1
+        return strs[0]
