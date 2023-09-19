f5504a0c5fea22b9afb17ab2d451be4ad823cbfb
SaberCon <894876785@qq.com>
2023-09-19 18:27:51 +0800

    Finish number-of-recent-calls

---
+++ leetcode-75/number-of-recent-calls.py
@@ -0,0 +1,13 @@
+from collections import deque
+
+
+class RecentCounter:
+
+    def __init__(self):
+        self.requests = deque()
+
+    def ping(self, t: int) -> int:
+        self.requests.append(t)
+        while self.requests[0] < t - 3000:
+            self.requests.popleft()
+        return len(self.requests)
