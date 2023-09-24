9cb8ba470b44de2ab10a8b91a45bce5581f195df
SaberCon <894876785@qq.com>
2023-09-25 07:50:23 +0800

    Finish binary-tree-right-side-view

---
+++ leetcode-75/binary-tree-right-side-view.py
@@ -0,0 +1,25 @@
+from typing import Optional, List
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        ans = []
+
+        def dfs(node: Optional[TreeNode], depth: int):
+            if not node:
+                return
+            if depth == len(ans):
+                ans.append(node.val)
+            dfs(node.right, depth + 1)
+            dfs(node.left, depth + 1)
+
+        dfs(root, 0)
+        return ans
