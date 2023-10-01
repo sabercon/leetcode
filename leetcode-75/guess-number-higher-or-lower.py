023b587ddf2ce47ab0459a087f41151d84243156
SaberCon <894876785@qq.com>
2023-10-02 07:56:56 +0800

    Finish guess-number-higher-or-lower

---
+++ leetcode-75/guess-number-higher-or-lower.py
@@ -0,0 +1,22 @@
+# The guess API is already defined for you.
+# @param num, your guess
+# @return -1 if num is higher than the picked number
+#          1 if num is lower than the picked number
+#          otherwise return 0
+def guess(num: int) -> int:
+    pass
+
+
+class Solution:
+    def guessNumber(self, n: int) -> int:
+        left, right = 1, n
+        while left <= right:
+            mid = (right + left) // 2
+            result = guess(mid)
+            if result == 1:
+                left = mid + 1
+            elif result == -1:
+                right = mid - 1
+            elif result == 0:
+                return mid
+        raise AssertionError
