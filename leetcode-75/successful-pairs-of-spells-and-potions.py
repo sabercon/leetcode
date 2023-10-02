ca13eb5bc51bb287677637ecad5eca9348d54f70
SaberCon <894876785@qq.com>
2023-10-02 08:07:46 +0800

    Finish successful-pairs-of-spells-and-potions

---
+++ leetcode-75/successful-pairs-of-spells-and-potions.py
@@ -0,0 +1,9 @@
+from bisect import bisect_left
+from math import ceil
+from typing import List
+
+
+class Solution:
+    def successfulPairs(self, spells: List[int], potions: List[int], success: int) -> List[int]:
+        potions.sort()
+        return [len(potions) - bisect_left(potions, ceil(success / spell)) for spell in spells]
