24a5cee78f4a90984c1d5b0a148d2f568b02e798
SaberCon <894876785@qq.com>
2023-10-02 14:24:19 +0800

    Add another solution for combination-sum-iii

--- leetcode-75/combination-sum-iii.py
+++ leetcode-75/combination-sum-iii.py
@@ -16,3 +16,21 @@ class Solution:
 
         dfs(list(range(1, 10)), k, n, [])
         return ans
+
+
+class Solution2:
+    def combinationSum3(self, k: int, n: int) -> List[List[int]]:
+        def dfs(nums: List[int], count: int, target: int) -> List[List[int]]:
+            if count == 0 and target == 0:
+                return [[]]
+            if not nums or count <= 0 or target <= 0:
+                return []
+
+            num, rest = nums[0], nums[1:]
+            ans_with_num = dfs(rest, count - 1, target - num)
+            for combination in ans_with_num:
+                combination.append(num)
+            ans_without_num = dfs(rest, count, target)
+            return ans_with_num + ans_without_num
+
+        return dfs(list(range(1, 10)), k, n)
