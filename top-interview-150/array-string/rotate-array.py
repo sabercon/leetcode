8ebc47a2ce4fa4de3c330f85e1a1a501d6d4e626
SaberCon <894876785@qq.com>
2023-10-10 09:23:10 +0800

    Finish rotate-array

---
+++ top-interview-150/array-string/rotate-array.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Can use reversion to solve rotation problems.
+
+        Do not return anything, modify nums in-place instead.
+        """
+
+        def reverse(start: int, end: int) -> None:
+            for i in range((end - start) // 2):
+                nums[start + i], nums[end - i - 1] = nums[end - i - 1], nums[start + i]
+
+        k = k % len(nums)
+        reverse(0, len(nums))
+        reverse(0, k)
+        reverse(k, len(nums))
