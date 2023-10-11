6dcdbdf57cba4a33ab253f1389a40a52c25ff894
SaberCon <894876785@qq.com>
2023-10-11 08:23:42 +0800

    Finish best-time-to-buy-and-sell-stock-ii

---
+++ top-interview-150/array-string/best-time-to-buy-and-sell-stock-ii.py
@@ -0,0 +1,6 @@
+from typing import List
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        return sum(prices[i + 1] - prices[i] for i in range(len(prices) - 1) if prices[i] < prices[i + 1])
