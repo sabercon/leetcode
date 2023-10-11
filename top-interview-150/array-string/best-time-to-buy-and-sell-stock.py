f6c79ddc89f4631063e396e9308bb84da0a2f255
SaberCon <894876785@qq.com>
2023-10-11 08:12:49 +0800

    Finish best-time-to-buy-and-sell-stock

---
+++ top-interview-150/array-string/best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        max_profit = 0
+        min_price = float('inf')
+
+        for price in prices:
+            max_profit = max(max_profit, price - min_price)
+            min_price = min(min_price, price)
+
+        return max_profit
