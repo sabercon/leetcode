e8a2a9bd1bca4cd719a9c87943bee6dcbfe4a6cd
SaberCon <894876785@qq.com>
2023-10-12 08:16:01 +0800

    Finish jump-game

---
+++ top-interview-150/array-string/jump-game.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        max_index = 0
+        for i, n in enumerate(nums):
+            if max_index < i:
+                return False
+            max_index = max(max_index, i + n)
+        return True
