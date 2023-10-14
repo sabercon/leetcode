f845af8fb6134050ff1a46ab7a0648f3fdfc3b60
SaberCon <894876785@qq.com>
2023-10-14 08:32:52 +0800

    Add another solution for candy

--- top-interview-150/array-string/candy.py
+++ top-interview-150/array-string/candy.py
@@ -11,3 +11,21 @@ class Solution:
             if i < len(ratings) - 1 and ratings[i + 1] < r:
                 candies[i] = max(candies[i], candies[i + 1] + 1)
         return sum(candies)
+
+
+class Solution2:
+    def candy(self, ratings: List[int]) -> int:
+        candies = last_peak = desc_size = 1
+        for i in range(1, len(ratings)):
+            if ratings[i] < ratings[i - 1]:
+                candies += desc_size if last_peak > desc_size else desc_size + 1
+                desc_size += 1
+                last_peak = max(last_peak, desc_size)
+            else:
+                if ratings[i] == ratings[i - 1]:
+                    last_peak = 1
+                else:
+                    last_peak = last_peak + 1 if desc_size == 1 else 2
+                desc_size = 1
+                candies += last_peak
+        return candies
