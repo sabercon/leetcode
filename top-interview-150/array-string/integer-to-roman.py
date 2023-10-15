4873a0188ed8b060f97c409aeca5684e3afc8d2c
SaberCon <894876785@qq.com>
2023-10-15 08:53:49 +0800

    Add another solution for integer-to-roman

--- top-interview-150/array-string/integer-to-roman.py
+++ top-interview-150/array-string/integer-to-roman.py
@@ -10,3 +10,12 @@ class Solution:
             num -= roman[-1][0]
             ans += roman[-1][1]
         return ans
+
+
+class Solution2:
+    def intToRoman(self, num: int) -> str:
+        ones = ('', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX')
+        tens = ('', 'X', 'XX', 'XXX', 'XL', 'L', 'LX', 'LXX', 'LXXX', 'XC')
+        huns = ('', 'C', 'CC', 'CCC', 'CD', 'D', 'DC', 'DCC', 'DCCC', 'CM')
+        thos = ('', 'M', 'MM', 'MMM')
+        return thos[num // 1000] + huns[num // 100 % 10] + tens[num // 10 % 10] + ones[num % 10]
