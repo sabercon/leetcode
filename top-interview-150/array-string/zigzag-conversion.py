9ad4ab9dafe1de494fffbe6f341cad0dd0dded5b
SaberCon <894876785@qq.com>
2023-10-16 09:22:42 +0800

    Add another solution for zigzag-conversion

--- top-interview-150/array-string/zigzag-conversion.py
+++ top-interview-150/array-string/zigzag-conversion.py
@@ -9,3 +9,16 @@ class Solution:
             if i == 0 or i == numRows - 1:
                 step *= -1
         return ''.join(rows)
+
+
+class Solution2:
+    def convert(self, s: str, numRows: int) -> str:
+        ans = []
+        num = max(2 * numRows - 2, 1)
+        for i in range(numRows):
+            for j in range(0, len(s), num):
+                if i + j < len(s):
+                    ans.append(s[i + j])
+                if i != 0 and i != numRows - 1 and num - i + j < len(s):
+                    ans.append(s[num - i + j])
+        return ''.join(ans)
