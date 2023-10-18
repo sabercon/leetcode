6daff0289db03c7f32e170dbaada7764154388a0
SaberCon <894876785@qq.com>
2023-10-18 08:33:07 +0800

    Finish two-sum-ii-input-array-is-sorted

---
+++ top-interview-150/two-pointers/two-sum-ii-input-array-is-sorted.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        left, right = 0, len(numbers) - 1
+        while numbers[left] + numbers[right] != target:
+            if numbers[left] + numbers[right] > target:
+                right -= 1
+            else:
+                left += 1
+        return [left + 1, right + 1]
