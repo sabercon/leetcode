67acbccc26e2ac02918e9cb5489c64a58ef69121
SaberCon <894876785@qq.com>
2023-10-24 09:01:36 +0800

    Add another solution for happy-number

--- top-interview-150/hashmap/happy-number.py
+++ top-interview-150/hashmap/happy-number.py
@@ -7,3 +7,16 @@ class Solution:
                 return False
             seen.add(n)
         return True
+
+
+class Solution2:
+    def isHappy(self, n: int) -> bool:
+        def sum_of_squares_of_digits(n: int) -> int:
+            return sum(int(i) ** 2 for i in str(n))
+
+        slow = n
+        fast = sum_of_squares_of_digits(n)
+        while slow != fast and fast != 1:
+            slow = sum_of_squares_of_digits(slow)
+            fast = sum_of_squares_of_digits(sum_of_squares_of_digits(fast))
+        return fast == 1
