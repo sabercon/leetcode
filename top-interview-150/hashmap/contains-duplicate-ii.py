fd8931bb73624ba9f2956e2289a589f33ec26b60
SaberCon <894876785@qq.com>
2023-10-25 09:29:35 +0800

    Finish contains-duplicate-ii

---
+++ top-interview-150/hashmap/contains-duplicate-ii.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        seen = {}
+        for i, num in enumerate(nums):
+            if num in seen and i - seen[num] <= k:
+                return True
+            seen[num] = i
+        return False
