128f08e1fe820472c3beb46fa05a04d3b58abf2b
SaberCon <894876785@qq.com>
2023-10-29 09:57:46 +0800

    Add another solution for min-stack

--- top-interview-150/stack/min-stack.py
+++ top-interview-150/stack/min-stack.py
@@ -18,3 +18,26 @@ class MinStack:
 
     def getMin(self) -> int:
         return self.min_stack[-1]
+
+
+class MinStack2:
+
+    def __init__(self):
+        self.stack = []
+        self.min = 2 ** 31 - 1
+
+    def push(self, val: int) -> None:
+        if val <= self.min:
+            self.stack.append(self.min)
+            self.min = val
+        self.stack.append(val)
+
+    def pop(self) -> None:
+        if self.min == self.stack.pop():
+            self.min = self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.min
