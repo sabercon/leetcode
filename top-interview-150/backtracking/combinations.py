f5f73276b2f1f681ffc88d98f5b3895146b35592
SaberCon <894876785@qq.com>
2023-11-10 08:44:10 +0800

    Add another solution for combinations

--- top-interview-150/backtracking/combinations.py
+++ top-interview-150/backtracking/combinations.py
@@ -16,3 +16,8 @@ class Solution:
 
         backtrack([0])
         return ans
+
+
+class Solution2:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        return [c + [i] for i in range(k, n + 1) for c in self.combine(i - 1, k - 1)] if k else [[]]
