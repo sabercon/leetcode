fe7ffb9521f8679b56f8a00a911fa79778b253f2
SaberCon <894876785@qq.com>
2023-11-12 09:43:53 +0800

    Add another solution for generate-parentheses

--- top-interview-150/backtracking/generate-parentheses.py
+++ top-interview-150/backtracking/generate-parentheses.py
@@ -10,3 +10,16 @@ class Solution:
         return [f'({p1}){p2}' for i in range(n)
                 for p1 in self.generateParenthesis(i)
                 for p2 in self.generateParenthesis(n - i - 1)]
+
+
+class Solution2:
+    def generateParenthesis(self, n: int) -> List[str]:
+        def generate(acc: str, left: int, right: int):
+            if right == 0:
+                yield acc
+            if left > 0:
+                yield from generate(acc + '(', left - 1, right)
+            if right > left:
+                yield from generate(acc + ')', left, right - 1)
+
+        return list(generate('', n, n))
