65012f54f612fb9406abb0b3414fe0992a32d486
SaberCon <894876785@qq.com>
2023-11-14 08:42:04 +0800

    Add another solution for merge-k-sorted-lists

--- top-interview-150/divide-and-conquer/merge-k-sorted-lists.py
+++ top-interview-150/divide-and-conquer/merge-k-sorted-lists.py
@@ -23,3 +23,26 @@ class Solution:
             if node.next:
                 heapq.heappush(heap, node.next)
         return dummy.next
+
+
+class Solution2:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        if not lists:
+            return None
+        if len(lists) == 1:
+            return lists[0]
+        mid = len(lists) // 2
+        return self.mergeTwoLists(self.mergeKLists(lists[:mid]), self.mergeKLists(lists[mid:]))
+
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = current = ListNode()
+        while list1 and list2:
+            if list1.val < list2.val:
+                current.next = list1
+                list1 = list1.next
+            else:
+                current.next = list2
+                list2 = list2.next
+            current = current.next
+        current.next = list1 or list2
+        return dummy.next
