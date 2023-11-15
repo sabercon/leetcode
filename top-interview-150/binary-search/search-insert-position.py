85e1a85babfed5504470a3b413b8f81f48d17bb3
SaberCon <894876785@qq.com>
2023-11-15 11:10:08 +0800

    Finish search-insert-position

---
+++ top-interview-150/binary-search/search-insert-position.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums)
+        while left < right:
+            mid = (left + right) // 2
+            if nums[mid] > target:
+                right = mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                return mid
+        return left
