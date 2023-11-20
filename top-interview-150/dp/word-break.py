d623d6d8e1e1ad744c1f0288df6f9e761dca31fb
SaberCon <894876785@qq.com>
2023-11-20 11:11:26 +0800

    Add another solution for word-break

--- top-interview-150/dp/word-break.py
+++ top-interview-150/dp/word-break.py
@@ -11,3 +11,12 @@ class Solution:
             return any(dp(i + len(word)) for word in wordDict if s.startswith(word, i))
 
         return dp(0)
+
+
+class Solution2:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        wordDict = set(wordDict)
+        dp = [True] + [False] * len(s)
+        for i in range(1, len(s) + 1):
+            dp[i] = any(dp[j] and s[j:i] in wordDict for j in range(0, i))
+        return dp[-1]
