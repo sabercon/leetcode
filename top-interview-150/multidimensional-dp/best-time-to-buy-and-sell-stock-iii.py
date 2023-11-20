05befcd8a5ee6464d72a2d6a2543f835716a21e1
SaberCon <894876785@qq.com>
2023-11-20 21:41:29 +0800

    Finish best-time-to-buy-and-sell-stock-iii

---
+++ top-interview-150/multidimensional-dp/best-time-to-buy-and-sell-stock-iii.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        buy1 = sell1 = buy2 = sell2 = float('-inf')
+        for price in prices:
+            buy1 = max(buy1, -price)
+            sell1 = max(sell1, buy1 + price)
+            buy2 = max(buy2, sell1 - price)
+            sell2 = max(sell2, buy2 + price)
+        return sell2
