fe57621361622cd25e59e61ac474cf2885baac26
Wenkai Yn <wenkaiyn@gmail.com>
2024-05-26 10:02:49 +0800

    Add another solution for longest-valid-parentheses

--- hard/longest-valid-parentheses.py
+++ hard/longest-valid-parentheses.py
@@ -12,3 +12,30 @@ class Solution:
                 stack.pop()
                 ans = max(ans, i - (stack[-1] if stack else start))
         return ans
+
+
+class Solution2:
+    def longestValidParentheses(self, s: str) -> int:
+        ans = 0
+        l = r = 0
+        for c in s:
+            if c == '(':
+                l += 1
+            else:
+                r += 1
+            if l == r:
+                ans = max(ans, l + r)
+            if l < r:
+                l = r = 0
+
+        l = r = 0
+        for c in reversed(s):
+            if c == '(':
+                l += 1
+            else:
+                r += 1
+            if l == r:
+                ans = max(ans, l + r)
+            if l > r:
+                l = r = 0
+        return ans
