b03c11b23076b24ea35f2dd862267e863fc2741e
Wenkai Yn <wenkaiyn@gmail.com>
2024-05-30 16:42:57 +0800

    Finish confirmation-rate

---
+++ sql-50/confirmation-rate.sql
@@ -0,0 +1,4 @@
+select s.user_id, round(avg(case when c.action = 'confirmed' then 1 else 0 end), 2) as confirmation_rate
+from Signups s
+         left join Confirmations c on s.user_id = c.user_id
+group by s.user_id
