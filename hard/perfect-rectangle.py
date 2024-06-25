ee5e2a423ec554a829d3fda5aed7f5f827ef03d9
Wenkai Yn <wenkaiyn@gmail.com>
2024-06-25 09:27:12 +0800

    Finish perfect-rectangle

---
+++ hard/perfect-rectangle.py
@@ -0,0 +1,27 @@
+from typing import List
+
+
+class Solution:
+    def isRectangleCover(self, rectangles: List[List[int]]) -> bool:
+        mx, my = min(r[0] for r in rectangles), min(r[1] for r in rectangles)
+        ma, mb = max(r[2] for r in rectangles), max(r[3] for r in rectangles)
+        lines = [(mx, my, mb, 'RIGHT'), (ma, my, mb, 'LEFT')]
+        for x, y, a, b in rectangles:
+            lines.append((x, y, b, 'LEFT'))
+            lines.append((a, y, b, 'RIGHT'))
+        lines.sort()
+
+        index, low, high, side = 0, 0, 0, 'LEFT'
+        for i, l, h, s in lines:
+            if low == high:
+                index, low, high, side = i, l, h, s
+            elif side == s:
+                if index != i or high != l:
+                    return False
+                high = h
+            else:
+                if index != i or low != l:
+                    return False
+                low, high = min(high, h), max(high, h)
+                side = side if high > h else s
+        return low == high
