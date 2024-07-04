ec6553118adf84aeab6ecaa5bdd150dbceb4e1e3
Wenkai Yn <wenkaiyn@gmail.com>
2024-07-04 08:38:38 +0800

    Finish poor-pigs

---
+++ hard/poor-pigs.py
@@ -0,0 +1,6 @@
+from math import log2, ceil
+
+
+class Solution:
+    def poorPigs(self, buckets: int, minutesToDie: int, minutesToTest: int) -> int:
+        return ceil(log2(buckets) / log2(minutesToTest // minutesToDie + 1))
