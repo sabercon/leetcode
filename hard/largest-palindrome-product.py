1f672769ace1b96139c0a7912cc70179623acd96
Wenkai Yn <wenkaiyn@gmail.com>
2024-07-13 18:25:34 +0800

    Finish largest-palindrome-product

--- hard/largest-palindrome-product.py
+++ hard/largest-palindrome-product.py
@@ -1,2 +1,15 @@
+from math import ceil
+
+
 class Solution:
     def largestPalindrome(self, n: int) -> int:
+        if n == 1:
+            return 9
+        top = 10 ** n - 1
+        # For `n` in [2,8], a palindrome of length `2*n` exists.
+        for left in range(top, top // 10, -1):
+            p = int(str(left) + str(left)[::-1])
+            for i in range(top, ceil(p ** 0.5) - 1, -1):
+                if p % i == 0:
+                    return p % 1337
+        raise AssertionError
